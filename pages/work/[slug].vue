812c8ee67245ee51b0b27c7cbce571ff00286223
Tamas Kuti <cooty13@gmail.com>
2022-11-09 17:37:08 +0200

    chore: add portfolio routes

---
+++ pages/work/[slug].vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>
+            This here is a portfolio item
+        </h1>
+        <p>
+            The slug is: <code>{{ slug }}</code>
+        </p>
+    </div>
+</template>
+
+<script setup lang="ts">
+const slug = ref<string>('')
+const route = useRoute()
+
+slug.value = route.params.slug as string
+
+useHead({
+    title: slug,
+    meta: [
+        { name: 'description', content: 'Some description from the DB' }
+    ]
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/work/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>
+            This here is the Portfolio's index page
+        </h1>
+        <ol>
+            <li>
+                <NuxtLink to="/work/takenaka-access-control">Access Control system for a major construction company
+                </NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/work/takenaka-worker-db">Health and safety compliance system for a major construction
+                    company</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/work/k-monitor-agrar">EU agricultural funds tracker for anti-corruption NGO</NuxtLink>
+            </li>
+        </ol>
+    </div>
+</template>
+
+<script setup>
+useHead({
+    title: 'Portfolio',
+    meta: [
+        { name: 'description', content: 'A collection of websites and applications that I\'ve worked on.' }
+    ]
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
