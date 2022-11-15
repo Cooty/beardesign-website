60821cd81016762f467632d42e84a2013de250f0
Tamas Kuti <cooty13@gmail.com>
2022-11-15 14:31:31 +0200

    chore: add label to theme switcher

---
+++ components/ui/UiCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <component :is="props.as" class="card">
+        <nuxt-link v-if="props.href" :to="props.href" class="ard-anchor" />
+    </component>
+</template>
+
+<script setup lang="ts">
+type AllowedTags = 'div' | 'article'
+const props = withDefaults(defineProps<{
+    as?: AllowedTags
+    href?: string,
+    external?: never
+}>(), {
+    as: 'article'
+})
+</script>
+
+<style lang="sass" scoped>
+
+</style>
