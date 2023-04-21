ed0562b70cf01430ac3b0cbad9b1e721a55c3aea
Tamas Kuti <cooty13@gmail.com>
2023-04-21 18:13:05 +0300

    feat: integrate work content

--- pages/work/[slug].vue
+++ pages/work/[slug].vue
@@ -1,26 +1,46 @@
 <template>
-    <LayoutMain>
-        <UiWrapper as="article" class="content-section" narrow>
-            <UiTitle :priority="1" sectionName="work">
-                This here is a portfolio item
-            </UiTitle>
-            <div class="content">
-                <p>
-                    The slug is: <code>{{ slug }}</code>
-                </p>
-            </div>
-        </UiWrapper>
-    </LayoutMain>
+    <layout-main>
+        <ui-wrapper as="article" class="content-section" narrow>
+            <template v-if="!error && !pending && data && data[0]">
+                <ui-title :priority="1" section-name="work">
+                    {{ data[0].title }}
+                </ui-title>
+                <ui-tags v-if="data[0].tags && data[0].tags.length" class="bd-mb-2">
+                    <ui-tag v-for="tag in data[0].tags" :key="tag.name" :text="tag.name" :type="tag.slug" as="span" />
+                </ui-tags>
+                <div v-if="blocks" class="content">
+                    <sanity-blocks :blocks="blocks" />
+                </div>
+                <template v-if="data[0].technologies">
+                    <ui-transition-into-view>
+                        <ui-title :priority="3" section-name="work" class="bd-text-center">
+                            Technologies used in the project
+                        </ui-title>
+                    </ui-transition-into-view>
+                    <ui-transition-into-view>
+                        <logo-grid>
+                            <carousel-item-card-logo v-for="tech in data[0].technologies" :key="tech.id" :name="tech.name"
+                                :image="tech.logo.src" :image-width="tech.logo.width" :image-height="tech.logo.height"
+                                :link="tech.website" />
+                        </logo-grid>
+                    </ui-transition-into-view>
+                </template>
+            </template>
+        </ui-wrapper>
+    </layout-main>
 </template>
 
 <script setup lang="ts">
+import { SanityBlocks } from 'sanity-blocks-vue-component';
 const slug = ref<string>('')
 const route = useRoute()
 
 slug.value = route.params.slug as string
+const { data, pending, error } = await useFetch(`/api/portfolio-item/${slug.value}`)
+const blocks = data.value[0].description
 
 useHead({
-    title: slug,
+    title: data.value[0].title,
     meta: [
         { name: 'description', content: 'Some description from the DB' }
     ]
